e70a888188962eb01c18c7effb0716ae03602f13
aleff menna resler <83561751+Aleff13@users.noreply.github.com>
2022-01-05 20:49:08 -0300

    Created a new Expo app

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.result}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
